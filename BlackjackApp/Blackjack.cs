9c0464721cb596ce34316c3eb37ef6341d5d6511
Danny Ahmad <43858798+daniql@users.noreply.github.com>
2024-09-05 21:49:09 -0400

    optimised conditionals to make more readable

--- BlackjackApp/Blackjack.cs
+++ BlackjackApp/Blackjack.cs
@@ -8,29 +8,25 @@
             Console.WriteLine("                  Welcome to Blackjack!");
             Console.WriteLine("                   Dealer draws to 16");
             Console.WriteLine("=============================================================");
+            
             var deck = CreateDeck();
             Shuffle(deck);
 
-            List<Card> playerHand = [];
-            List<Card> dealerHand = [];
-
-            playerHand.Add(deck.Deal());
-            Console.WriteLine($"Dealer has dealt you a {playerHand.Last()}");
-            playerHand.Add(deck.Deal());
-            Console.WriteLine($"Dealer has dealt you a {playerHand.Last()}");
-            dealerHand.Add(deck.Deal());
+            var playerHand = new List<Card> { deck.Deal(), deck.Deal()};
+            var dealerHand = new List<Card> { deck.Deal(), deck.Deal()};
+            
+            Console.WriteLine($"Dealer has dealt you a {playerHand[0]}");
+            Console.WriteLine($"Dealer has dealt you a {playerHand[1]}");
 
             var playerHandStartingValue = GetHandValue(playerHand);
 
             if (playerHandStartingValue != 21)
             {
-                Console.WriteLine($"Dealer has shown a {dealerHand.Last()}");
-                var dealerShownValue = dealerHand.Last().Value;
-                dealerHand.Add(deck.Deal());
+                Console.WriteLine($"Dealer has shown a {dealerHand[0]}");
                 Console.WriteLine("Dealer has drawn a card and placed it face down");
                 Console.WriteLine("=============================================================");
                 
-                var playerBusted = PlayerTurn(deck, playerHand, dealerShownValue);
+                var playerBusted = PlayerTurn(deck, playerHand, dealerHand[0].Value);
             
                 if (!playerBusted)
                 {
@@ -54,15 +50,7 @@
 
             if (dealerHandValue > 21 || playerHandValue > dealerHandValue)
             {
-                if (dealerHandValue > 21)
-                {
-                    Console.WriteLine("Dealer busts!");
-                }
-                else
-                {
-                    Console.WriteLine($"You have {playerHandValue} and the dealer has {dealerHandValue}");
-                }
-
+                Console.WriteLine(dealerHandValue > 21 ? "Dealer busted!" : $"You have {playerHandValue} and the dealer has {dealerHandValue}");
                 Console.WriteLine("You win!");
             }
             else if (dealerHandValue > playerHandValue)
@@ -79,29 +67,19 @@
 
         private static void DealerTurn(Deck deck, List<Card> dealerHand)
         {
-            Console.WriteLine($"Dealer reveals their face down card, it's a {dealerHand.Last()}!");
+            Console.WriteLine($"Dealer reveals their face down card, it's a {dealerHand[1]}!");
             var handValue = GetHandValue(dealerHand);
             Console.WriteLine($"Value of dealer's hand is {handValue}");
-            while (true)
+
+            while (handValue < 16)
             {
-                switch(handValue)
-                {
-                    case > 21:
-                        Console.WriteLine("=============================================================");
-                        return;
-                    case >= 16:
-                        Console.WriteLine("Dealer's hand is at or above 16 and the dealer stands");
-                        Console.WriteLine("==================================================");
-                        return;
-                    default:
-                        Console.WriteLine("=============================================================");
-                        dealerHand.Add(deck.Deal());
-                        Console.WriteLine($"Dealer draws a {dealerHand.Last()}");
-                        handValue = GetHandValue(dealerHand);
-                        Console.WriteLine($"Value of dealer's hand is {handValue}");
-                        break;  
-                }
+                dealerHand.Add(deck.Deal());
+                Console.WriteLine($"Dealer draws a {dealerHand.Last()}");
+                handValue = GetHandValue(dealerHand);
+                Console.WriteLine($"Value of dealer's hand is {handValue}");
             }
+            Console.WriteLine(handValue > 21 ? "Dealer busts!" : "Dealer stands");
+            Console.WriteLine("=============================================================");
         }
 
         private static bool PlayerTurn(Deck deck, List<Card> playerHand, Value dealerShownValue)
@@ -109,54 +87,41 @@
             while (true)
             {
                 var handValue = GetHandValue(playerHand);
+                
                 Console.WriteLine($"Dealer is showing a {dealerShownValue}");
                 Console.WriteLine($"The current value of your hand is {handValue}");
+                
                 if (handValue > 21)
                 {
                     Console.WriteLine("You busted!");
                     return true;
                 }
+                
                 Console.WriteLine("Do you want to 'hit' or 'stand'?");
                 var input = Console.ReadLine()?.ToLower();
+                
                 switch (input)
                 {
                     case "hit":
                         playerHand.Add(deck.Deal());
                         Console.WriteLine($"The dealer deals you a {playerHand.Last()}");
-                        Console.WriteLine("=============================================================");
                         break;
                     case "stand":
                         Console.WriteLine("=============================================================");
                         return false;
                     default:
                         Console.WriteLine("Invalid input: Please enter either 'hit' or 'stand'");
-                        Console.WriteLine("=============================================================");
                         break;
                 }
+                Console.WriteLine("=============================================================");
+
             }
         }
 
         private static int GetHandValue(List<Card> hand)
         {
-            var value = 0;
-            var numOfAces = 0;
-
-            foreach (var card in hand)
-            {
-                if (card.Value == Value.Ace)
-                {
-                    value += 11;
-                    numOfAces++;
-                }
-                else if (card.Value > Value.Ten)
-                {
-                    value += 10;
-                }
-                else
-                {
-                    value += (int)card.Value;
-                }
-            }
+            var value = hand.Sum(card => card.Value == Value.Ace ? 11 : card.Value > Value.Ten ? 10 : (int)card.Value);
+            var numOfAces = hand.Count(card => card.Value == Value.Ace);
 
             while (value > 21 && numOfAces > 0)
             {
@@ -183,16 +148,7 @@
         private static void Shuffle(Deck deck)
         {
             var rnd = new Random();
-            var n = deck.Cards.Count();
-            while (n > 1)
-            {
-                n--;
-                var k = rnd.Next(n+1);
-                (deck.Cards[k], deck.Cards[n]) = (deck.Cards[n], deck.Cards[k]);
-            }
+            deck.Cards = deck.Cards.OrderBy(_ => rnd.Next()).ToList();
         }
     }
 }
-
-
-
